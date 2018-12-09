24be22aaf6f1b75be4ed4ce2c6cc73d58daad464
Junior Marques <gilmar_junior@hotmail.com.br>
2018-12-09 16:39:18 +0000

    projeto Value Default v2.1.2 - menu, menu carregando a pagina via javascript

---
+++ js/mainContent.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a').forEach(link =>{
+    const content = document.getElementById("mainContent")
+
+    link.onclick = function(e){
+        e.preventDefault()
+
+        fetch(link.href)
+            .then(resp => resp.text())
+            .then(html => content.innerHTML = html)
+    }
+
+})
+
+
